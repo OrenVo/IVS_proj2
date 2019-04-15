288bf00afb3e1c69c68c5d4eba381e10f3f806f7
pszepsi <49364963+pszepsi@users.noreply.github.com>
2019-04-15 20:18:33 +0200

    Added documentation

--- src/deus_profiling.py
+++ src/deus_profiling.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python
 
+
+###############################################################################
+# @file deus_profiling.c
+# @brief Calculation of the standard deviation of numbers from the inputfile.
+#
+# @author Pavol Szepsi
+#
+# @version 1.7.0
+# @date 15.4.2019
+###############################################################################
+
+
 import sys
 import deus_math_lib
 
+
+##
+# @brief Function calculates standard deviation.
+#
+# @return Standard deviation of numbers from input.
 def StandardDeviation():
     sumX = 0
     sumX2 = 0
@@ -18,3 +35,9 @@ def StandardDeviation():
     return deus_math_lib.deus_root((1 / (N - 1) * (sumX2 - (N * deus_math_lib.deus_pow(avgX, 2)))), 2)
 
 print(StandardDeviation())
+
+
+###############################################################################
+# @}
+# End of the deus_profiling.py
+###############################################################################
