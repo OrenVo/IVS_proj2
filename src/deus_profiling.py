ae55a637d905063da91a103258785f409f382db5
xfulla00 <xfulla00@stud.fit.vutbr.cz>
2019-04-15 20:36:54 +0200

    -Repairing my damages

--- src/deus_profiling.py
+++ src/deus_profiling.py
@@ -16,10 +16,6 @@ import sys
 import deus_math_lib
 
 
-##
-# @brief Function calculates standard deviation.
-#
-# @return Standard deviation of numbers from input.
 def StandardDeviation():
     sumX = 0
     sumX2 = 0
@@ -33,6 +29,7 @@ def StandardDeviation():
 
     avgX = sumX / N
     return deus_math_lib.deus_root((1 / (N - 1) * (sumX2 - (N * deus_math_lib.deus_pow(avgX, 2)))), 2)
+
 
 print(StandardDeviation())
 
